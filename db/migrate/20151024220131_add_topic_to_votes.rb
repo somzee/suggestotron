8e733cce31530cd49b75daf2e22369333f7d46db
Sowmya Ranganathan <rsowmya90@gmail.com>
2015-10-24 19:53:41 -0700

    commented out add topic class for heroku

--- db/migrate/20151024220131_add_topic_to_votes.rb
+++ db/migrate/20151024220131_add_topic_to_votes.rb
@@ -1,7 +1,7 @@
 class AddTopicToVotes < ActiveRecord::Migration
-  def change
-  	change_table :votes do |t|
-  		t.integer :topic_id, null: false
-  	end
+  # def change
+  # 	change_table :votes do |t|
+  # 		t.integer :topic_id, null: false
+  # 	end
   end
 end
